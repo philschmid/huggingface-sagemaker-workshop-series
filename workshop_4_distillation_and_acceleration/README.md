66e581b264026f7c6325d160df9b0e508b99db7f
philschmid <schmidphilipp1995@gmail.com>
2022-04-11 10:55:52 +0200

    v0.1

---
+++ workshop_4_distillation_and_acceleration/README.md
@@ -0,0 +1,15 @@
+## Workshop 4: **Accelerate BERT Inference with Knowledge Distillation & AWS Inferentia**
+
+In the workshop, you will learn how to apply knowledge distillation to compress a large model to a small model, and then from the small model to an optimized neuron model with AWS Inferentia. By the end of this process, our model will go from 100ms+ to 5ms+ latency - a 20x improvement! 🤯 🏎
+
+
+- Apply knowledge-distillation with BERT-large as teacher and MiniLM as student
+- Compile a Hugging Face Transformer model with AWS Neuron for AWS Inferentia
+- Deploy the distilled & optimized model to Amazon SageMaker for production-grade fast inference
+
+
+---
+
+🧑🏻‍💻 Code Assets: [workshop_4_distillation_and_acceleration](https://github.com/philschmid/huggingface-sagemaker-workshop-series/tree/main/workshop_4_distillation_and_acceleration)
+
+📺 Youtube: _Coming Soon_
---
+++ workshop_4_distillation_and_acceleration/visualize.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def plot_metrics(perf_metrics, current_optim_type):
+    df = pd.DataFrame.from_dict(perf_metrics, orient='index')
+
+    for idx in df.index:
+        df_opt = df.loc[idx]
+        # Add a dashed circle around the current optimization type
+        if idx == current_optim_type:
+            plt.scatter(df_opt["time_p99_ms"], df_opt["accuracy"] * 100, 
+                        alpha=0.5, s=df_opt["size_mb"], label=idx, 
+                        marker='$\u25CC$')
+        else:
+            plt.scatter(df_opt["time_p99_ms"], df_opt["accuracy"] * 100, 
+                        s=df_opt["size_mb"], label=idx, alpha=0.5)
+            
+    legend = plt.legend(bbox_to_anchor=(1,1))
+    for handle in legend.legendHandles:
+        handle.set_sizes([20])
+
+    plt.ylim(90,98)
+    # Use the slowest model to define the x-axis range
+    xlim = int(perf_metrics["roberta-large"]["time_p99_ms"] + 3)
+    plt.xlim(1, xlim+20)
+    plt.ylabel("Accuracy (%)")
+    plt.xlabel("p99 latency (ms)")
+    plt.show()
