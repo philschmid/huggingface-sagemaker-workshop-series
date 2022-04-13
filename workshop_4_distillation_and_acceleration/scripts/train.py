94acea8642bef9256a105ef889b6f121c24c022b
Philipp Schmid <32632186+philschmid@users.noreply.github.com>
2022-04-13 14:07:17 +0200

    Workshop 4: Accelerate BERT Inference  (#6)
    * v0.1
    
    * updated performance
    
    * removed different deploy
    
    * remove os env
    
    * add inferentia notebook
    
    * add file size calculation
    
    * Update dataset and model IDs for CLINC
    
    * add visual
    
    * Add images, reworks nbs
    
    * updated visuale
    
    * removed unimportant outputs and added description
    
    * added images
    
    * added header
    
    * removed push to hub
    
    * remove output
    
    * changes
    
    * Kill push to hub
    
    * fixed push to hub again
    
    * Resurrect push to hub
    
    * added notebook login
    
    * added upgrade for pip
    
    * adding job name
    
    * fixed link
    
    * added better images
    
    Co-authored-by: Lewis Tunstall <lewis.c.tunstall@gmail.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-16-66-96.ec2.internal>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-16-74-188.ec2.internal>
    Co-authored-by: Lewis Tunstall <lewis@huggingface.co>

---
+++ workshop_4_distillation_and_acceleration/scripts/train.py
@@ -0,0 +1,137 @@
+import argparse
+import logging
+import os
+from pathlib import Path
+import sys
+import time
+
+import numpy as np
+from datasets import load_dataset, load_metric
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    # hyperparameters sent by the client are passed as command-line arguments to the script.
+    parser.add_argument("--epochs", type=int, default=3)
+    parser.add_argument("--train_batch_size", type=int, default=32)
+    parser.add_argument("--eval_batch_size", type=int, default=64)
+    parser.add_argument("--dataset_id", type=str)
+    parser.add_argument("--dataset_config", type=str)
+    parser.add_argument("--warmup_steps", type=int, default=500)
+    parser.add_argument("--model_id", type=str)
+    parser.add_argument("--learning_rate", type=str, default=5e-5)
+    parser.add_argument("--fp16", type=bool, default=True)
+
+    # Push to Hub Parameters
+    parser.add_argument("--push_to_hub", type=bool, default=True)
+    parser.add_argument("--hub_model_id", type=str, default=None)
+    parser.add_argument("--hub_strategy", type=str, default="every_save")
+    parser.add_argument("--hub_token", type=str, default=None)
+
+    args, _ = parser.parse_known_args()
+
+    # Set up logging
+    logger = logging.getLogger(__name__)
+
+    logging.basicConfig(
+        level=logging.getLevelName("INFO"),
+        handlers=[logging.StreamHandler(sys.stdout)],
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+
+    # Load tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model_id)
+
+    # load datasets
+    dataset = load_dataset(args.dataset_id, args.dataset_config)
+
+    # process dataset
+    def process(examples):
+        tokenized_inputs = tokenizer(examples["text"], truncation=True, max_length=512)
+        return tokenized_inputs
+
+    tokenized_datasets = dataset.map(process, batched=True)
+    tokenized_datasets = tokenized_datasets.rename_column("intent", "labels")
+
+    metric = load_metric("accuracy")
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        predictions = np.argmax(predictions, axis=1)
+        return metric.compute(predictions=predictions, references=labels)
+
+
+    # create label2id, id2label dicts for nice outputs for the model
+    labels = tokenized_datasets["train"].features["labels"].names
+    num_labels = len(labels)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+
+    # download model from model hub
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model_id, num_labels=num_labels, label2id=label2id, id2label=id2label
+    )
+        
+    # define training args
+    output_dir = Path("/opt/ml/output/data")
+    training_args = TrainingArguments(
+        output_dir=output_dir.as_posix(),
+        num_train_epochs=args.epochs,
+        per_device_train_batch_size=args.train_batch_size,
+        per_device_eval_batch_size=args.eval_batch_size,
+        warmup_steps=args.warmup_steps,
+        fp16=args.fp16,
+        learning_rate=float(args.learning_rate),
+        # logging & evaluation strategies
+        logging_dir=f"{output_dir.as_posix()}/logs",
+        logging_strategy="epoch",  # to get more information to TB
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        save_total_limit=2,
+        load_best_model_at_end=True,
+        metric_for_best_model="accuracy",
+        report_to="tensorboard",
+        # push to hub parameters
+        push_to_hub=args.push_to_hub,
+        hub_strategy=args.hub_strategy,
+        hub_model_id=args.hub_model_id,
+        hub_token=args.hub_token,
+    )
+
+    # create Trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["validation"],
+        tokenizer=tokenizer,
+    )
+
+    # train model
+    trainer.train()
+
+    # evaluate model
+    eval_result = trainer.evaluate(eval_dataset=tokenized_datasets["validation"])
+
+    # writes eval result to file which can be accessed later in s3 ouput
+    with open(os.path.join(output_dir.as_posix(), "eval_results.txt"), "w") as writer:
+        print(f"***** Eval results *****")
+        for key, value in sorted(eval_result.items()):
+            writer.write(f"{key} = {value}\n")
+            print(f"{key} = {value}\n")
+
+    # save best model, metrics and create model card
+    if args.push_to_hub:
+        trainer.create_model_card(model_name=args.hub_model_id)
+        # wait for asynchronous pushes to finish
+        time.sleep(180)
+        trainer.push_to_hub()
+        
+    # Saves the model to s3 uses os.environ["SM_MODEL_DIR"] to make sure checkpointing works
+    trainer.save_model(os.environ["SM_MODEL_DIR"])
---
+++ workshop_4_distillation_and_acceleration/visualize.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def plot_metrics(perf_metrics, current_optim_type):
+    df = pd.DataFrame.from_dict(perf_metrics, orient="index")
+
+    for idx in df.index:
+        df_opt = df.loc[idx]
+        # Add a dashed circle around the current optimization type
+        if idx == current_optim_type:
+            plt.scatter(
+                df_opt["time_p99_ms"],
+                df_opt["accuracy"] * 100,
+                alpha=0.5,
+                s=df_opt["size_mb"],
+                label=idx,
+                marker="$\u25CC$",
+            )
+        else:
+            plt.scatter(df_opt["time_p99_ms"], df_opt["accuracy"] * 100, s=df_opt["size_mb"], label=idx, alpha=0.5)
+
+    legend = plt.legend(bbox_to_anchor=(1, 1))
+    for handle in legend.legendHandles:
+        handle.set_sizes([20])
+
+    plt.ylim(80, 100)
+    # Use the slowest model to define the x-axis range
+    xlim = int(perf_metrics["roberta-large"]["time_p99_ms"] + 30)
+    plt.xlim(0, xlim)
+    plt.ylabel("Accuracy (%)")
+    plt.xlabel("p99 latency (ms)")
+    plt.show()
